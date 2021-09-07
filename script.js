a9210765f63106b3fe099f4404423a25e2d3056a
Sanjik761 <sanjarburiniyozov@gmail.com>
2021-09-08 00:03:26 +0500

    querySelector homesec and homePort

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 const home = document.querySelector('.home');
 const about = document.querySelector('.about');
 const contact = document.querySelector('.contact');
+const homeSec = document.querySelector('.homeSec');
+const homePort = document.querySelector('.homePort');
