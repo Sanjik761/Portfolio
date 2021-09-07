d8ef094119e8d34e4b66e9e297b80cbeda2f4bd3
Sanjik761 <sanjarburiniyozov@gmail.com>
2021-09-08 00:08:57 +0500

    modified js

--- script.js
+++ script.js
@@ -18,14 +18,14 @@ home.addEventListener('click', () => {
 about.addEventListener('click', () => {
   homePort.style.cssText = "display: none"
   homeSec.style.cssText = "display: none"
- aboutPort.style.cssText = "display: block; background: none"
+  aboutPort.style.cssText = "display: block; background: none"
   contactPort.style.cssText = "display: none"
   footer.style.cssText = "display: flex; justify-content: space-between; margin: 0 50px; color: rgb(100, 100, 100); font-family: sans-serif; font-size: 15px;"
 });
 
 contact.addEventListener('click', () => {
   aboutPort.style.cssText = "display: none"
- homePort.style.cssText = "display: none"
+  homePort.style.cssText = "display: none"
   homeSec.style.cssText = "display: none"
   contactPort.style.cssText = "display: block; background-size: 110%; background-image: url(https://www.catenon.com/front/img/backgrounds/bg-contact.jpg); background-repeat: no-repeat"
 });
