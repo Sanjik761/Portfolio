215ac09ea27c9825908c08bc8d833dbb7fb6c8a1
Sanjik761 <sanjarburiniyozov@gmail.com>
2021-09-08 00:06:05 +0500

    created home function

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ const homePort = document.querySelector('.homePort');
 const aboutPort = document.querySelector('.aboutPort');
 const contactPort = document.querySelector('.contactPort');
 const footer = document.querySelector('.footer');
+
+
+home.addEventListener('click', () => {
+  homeSec.style.cssText = "display: none"
+  aboutPort.style.cssText = "display: none"
+  contactPort.style.cssText = "display: none"
+  homePort.style.cssText = "display: block"
+});
